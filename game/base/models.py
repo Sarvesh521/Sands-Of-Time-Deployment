f4a00c15a1cdde3e99190bee7fd07d6ccad79168
Sarvesh521 <122361969+Sarvesh521@users.noreply.github.com>
2025-07-28 18:27:04 +0530

    Deploying

--- game/base/models.py
+++ game/base/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import AbstractUser
 from datetime import datetime
 class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
-    phone_number=models.IntegerField(null=True)
-    username=models.CharField(unique=True,max_length=100,null=True)
-    gold=models.IntegerField(default=0)
-    sand=models.DateTimeField(default=datetime(2025,10,27,12,5,30))
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    phone_number = models.IntegerField(null=True)
+    username = models.CharField(unique=True, max_length=100, null=True)
+    gold = models.IntegerField(default=0)
+    sand = models.DateTimeField(default=datetime(2025, 7, 30, 17, 30, 0))
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
     def __str__(self) -> str:
         return str(self.email)
 
